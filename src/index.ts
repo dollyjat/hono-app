d4e99041db3b26299079a8e03327917aaaba2637
dollyjat <jattgaurav293@gmail.com>
2025-06-30 07:29:17 +0530

    feat: add /user rout

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,21 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+	return c.text('Hello Hono!')
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+app.get('/user', (c) => {
+	return c.json({
+		name: 'Gaurav Jatt',
+	})
 })
+
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`)
+	}
+)
